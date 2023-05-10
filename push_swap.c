9d0c13ff3c357fde6ccf8749dc3ee4b32df5c602
fabileo <76965192+leonfabi@users.noreply.github.com>
2023-05-10 20:43:27 +0200

    add pushswap

---
+++ push_swap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 20:22:42 by fkrug             #+#    #+#             */
+/*   Updated: 2023/05/10 20:40:20 by fkrug            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./Libft/libft.h"
+
+int	input_check(int argc, char **argv)
+{
+	int	count;
+
+	count = 0;
+	while (++count < argc)
+	{
+		
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	// if (argc < 2)
+	// 	return (EXIT_SUCCESS);
+	input_check(argc, argv);
+	return (EXIT_SUCCESS);
+}
