4d05d0a060fe2414241e6469ffa091e00f074283
42fabileo <76965192+leonfabi@users.noreply.github.com>
2023-05-13 12:09:40 +0200

    init function tracks length of initial stack

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 20:22:42 by fkrug             #+#    #+#             */
-/*   Updated: 2023/05/13 10:48:44 by fkrug            ###   ########.fr       */
+/*   Updated: 2023/05/13 12:09:09 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 int	initialize_stack(int argc, char **argv, t_s *stp)
 {
 	int		i;
+	int		length;
 	char	**tmp;
 
 	i = 0;
+	length = 0;
 	while (argc-- > 1)
 	{
 		tmp = ft_split(argv[argc], ' ');
@@ -28,6 +30,7 @@ int	initialize_stack(int argc, char **argv, t_s *stp)
 			if (ft_error(stp, tmp, !is_str_valid(tmp[i])))
 				return (0);
 			ft_lstadd_front(&(stp->sa), ft_lstnew(ft_cnew(ft_atoi(tmp[i]), 0)));
+			length++;
 			i++;
 		}
 		if (ft_error(stp, tmp, ft_list_double(stp->sa)))
@@ -35,18 +38,19 @@ int	initialize_stack(int argc, char **argv, t_s *stp)
 		else
 			ft_free_2d(tmp);
 	}
-	return (0);
+	return (length);
 }
 
 int	main(int argc, char **argv)
 {
 	t_s	stacks;
+	int	length;
 
 	stacks.sa = NULL;
 	stacks.sb = NULL;
 	if (argc < 2)
 		return (EXIT_FAILURE);
-	initialize_stack(argc, argv, &stacks);
+	length = initialize_stack(argc, argv, &stacks);
 	ft_sanitize(&stacks);
 	return (EXIT_SUCCESS);
 }
